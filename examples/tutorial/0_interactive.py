e2635545e7d1f3b1774c262a34e85a30153433cb
Achazwl <acha131441373@gmail.com>
2022-06-10 17:09:45 +0800

    fix strip

--- examples/tutorial/0_interactive.py
+++ examples/tutorial/0_interactive.py
@@ -2,8 +2,15 @@ from transformers import BertForMaskedLM
 model = BertForMaskedLM.from_pretrained("bert-base-cased")
 # suppose we load BERT
 
+import sys
+
+if len(sys.argv) == 1:
+    port=True
+else:
+    port=int(sys.argv[1])
+
 from opendelta import LoraModel
-delta_model = LoraModel(backbone_model=model, interactive_modify=True)
+delta_model = LoraModel(backbone_model=model, interactive_modify=port)
 # This will visualize the backbone after modification and other information.
 
 delta_model.freeze_module(exclude=["deltas", "layernorm_embedding"], set_state_dict=True)
